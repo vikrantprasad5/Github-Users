f489d36919595919925ca0b3c348a3597538033f
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-08-28 20:47:49 +0530

    first commit

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,15 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+//Provider, consumer - GithubContext.Provider
+const GithubProvider = ({children})=>{
+    const [githubUser,setGithubUser]=useState(mockUser);
+    const [repos,setRepos]=useState(mockRepos);
+    const [followers,setFollowers]=useState(mockFollowers);
+    return (<GithubContext.Provider value={{githubUser,repos,followers}}>{children}</GithubContext.Provider>);
+};
+
+export{GithubProvider, GithubContext};
