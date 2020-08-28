6ae5cff4fa821923149d7e92af104a7f022ba304
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-08-29 02:02:36 +0530

    Fusion charts API dynamically passing data to  chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,25 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const {repos} = React.useContext(GithubContext);
   console.log(repos);
-  return <ExampleChart/>;
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13"
+    },
+    {
+      label: "CSS",
+      value: "400"
+    },
+    {
+      label: "Javascript",
+      value: "80"
+    }
+  ];
+  return <section className="section">
+    <Wrapper className="section-center">
+      <ExampleChart data={chartData}/>
+    </Wrapper>
+  </section>;
 };
 
 const Wrapper = styled.div`
